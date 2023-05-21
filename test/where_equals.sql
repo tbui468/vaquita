7a2d3c50132a9c7ab7db2c893d2d71cc9374d4bd
thomas <thomasthienbui@gmail.com>
2023-05-21 22:38:36 +0700

    Added more tests

---
+++ test/where_equals.sql
@@ -0,0 +1,12 @@
+create database sol;
+open sol;
+
+create table planets (name string, moons int, rings bool);
+insert into planets (name, moons, rings) values ("Mars", 2, true), ("Earth", 1, false);
+select * from planets where name = "Earth";
+select * from planets where moons = 1;
+select * from planets where rings = false;
+
+close sol;
+drop database sol;
+exit;
